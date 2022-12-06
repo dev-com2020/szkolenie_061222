746504d56224f6468d5f90b59c493eee9ab228d2
devcom2020 <kwp@tlen.pl>
2022-12-06 13:01:31 +0100

    wtorek 13:01

---
+++ slowniki.py
@@ -0,0 +1,17 @@
+slownik = {"gracz": {"imie": "Tomek", "wiek": 39},
+           "punkty": [33, 44, 55, 66],
+           "czy_pali": False}
+
+slownik["przerwa"] = True
+slownik["czy_pali"] = None
+
+print(slownik)
+print(slownik['gracz'])
+print(slownik['gracz']['imie'])
+print(slownik['punkty'][2])
+print(slownik.values())
+print(slownik.keys())
+print(slownik.items())
+print(slownik.get('gracz'))
+
+
