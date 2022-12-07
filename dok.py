3835f711ccb42448c04afda241406a3959751d3f
devcom2020 <kwp@tlen.pl>
2022-12-07 11:56:50 +0100

    sroda 11:56

---
+++ dok.py
@@ -0,0 +1,13 @@
+def connect(host, port, user, password):
+   """
+   Funkcja łączy się z bazą danych
+   :param host: adres IP
+   :param port: port bazy
+   :param user: użytkownik
+   :param password: hasło
+   :return: połączenie do bazy
+   """
+   conn = None
+   return conn
+
+
