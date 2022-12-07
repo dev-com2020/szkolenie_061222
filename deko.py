759b9c3badf9b894e44d439fe5330bbc6ec639f0
devcom2020 <kwp@tlen.pl>
2022-12-07 11:39:19 +0100

    sroda 11:39

--- deko.py
+++ deko.py
@@ -1,3 +1,6 @@
+import time
+
+
 def wykonaj(funkcja, a, b):
     x = funkcja(a, b)
     return x
@@ -26,18 +29,17 @@ def dodaj(a, b):
     return a + b
 
 
-def odejmij(a, b):
-    return a - b
-
-
 print(wykonaj(dodaj, 2, 3))
 print(glowna())
 
 
 def dekor(funkcja):
     def wew(*args, **kwargs):
-        print("Dekorujemy funkcje")
-        return funkcja(*args, **kwargs)
+        start = time.time()
+        x = funkcja(*args, **kwargs)
+        koniec = time.time()
+        print(funkcja.__name__, "wykonywała się: ", koniec - start)
+        return x
 
     return wew
 
@@ -45,7 +47,14 @@ def dekor(funkcja):
 @dekor
 def zwykla(a, b, c):
     print("To jest zwykła funkcja")
+    time.sleep(2)
     print(a + b + c)
 
 
-zwykla(1,2,3)
+@dekor
+def odejmij(a, b):
+    return a - b
+
+
+zwykla(1, 2, 3)
+odejmij(50, 4)
