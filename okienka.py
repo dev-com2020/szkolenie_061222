590d6c5c59789eca4dd03e993696e1f5aa958658
devcom2020 <kwp@tlen.pl>
2022-12-09 15:42:30 +0100

    piatek 15:42

--- okienka.py
+++ okienka.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QMenu, \
+    QAction
 import sys
 
 
@@ -7,20 +8,36 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Moja aplikacja')
-        self.button = QPushButton("Wcisnij mnie!")
+        # self.button = QPushButton("Wcisnij mnie!")
         self.setFixedSize(QSize(400, 300))
-        self.setCentralWidget(self.button)
-        self.button.clicked.connect(self.button_was_clicked)
-        self.button.clicked.connect(self.button_was_toggled)
-        self.button.setCheckable(True)
+        # self.setCentralWidget(self.button)
+        # self.button.clicked.connect(self.button_was_clicked)
+        # self.button.clicked.connect(self.button_was_toggled)
+        # self.button.setCheckable(True)
+        # self.label = QLabel()
+        # self.input = QLineEdit()
+        # self.input.textChanged.connect(self.label.setText)
+        # layout = QVBoxLayout()
+        # layout.addWidget(self.input)
+        # layout.addWidget(self.label)
+        # container = QWidget()
+        # container.setLayout(layout)
+        # self.setCentralWidget(container)
 
-    def button_was_clicked(self):
-        print('Kliknięto!')
-        self.button.setText('Już mnie kliknąłeś!!!')
-        self.setWindowTitle('Super apka!')
-
-    def button_was_toggled(self, checked):
-        print("Checked?", checked)
+    def contextMenuEvent(self, e):
+        context = QMenu()
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
+    #
+    # def button_was_clicked(self):
+    #     print('Kliknięto!')
+    #     self.button.setText('Już mnie kliknąłeś!!!')
+    #     self.setWindowTitle('Super apka!')
+    #
+    # def button_was_toggled(self, checked):
+    #     print("Checked?", checked)
 
 
 app = QApplication(sys.argv)
